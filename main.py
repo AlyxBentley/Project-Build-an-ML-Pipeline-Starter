16f282d7fc161d8b7f1be1bb85a534ead83ce26d
Alexandra Bentley <abent97@wgu.edu>
2026-02-03 17:13:14 -0800

    corrected indentation on random forest training test

--- main.py
+++ main.py
@@ -110,20 +110,20 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
-        _ = mlflow.run(
-            os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
-            "main",
-            env_manager="conda",
-            parameters={
-                "trainval_artifact": "trainval_data.csv:latest",
-                "val_size": config["modeling"]["val_size"],
-                "random_seed": config["modeling"]["random_seed"],
-                "stratify_by": config["modeling"]["stratify_by"],
-                "rf_config": rf_config,
-                "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                "output_artifact": "random_forest_export",
-            },
-        )
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                env_manager="conda",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export",
+                },
+            )
 
 
         if "test_regression_model" in active_steps:
@@ -132,7 +132,16 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
-            pass
+            """_ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_artifact": "test_data.csv:latest",
+                },
+            )"""
+
 
 
 if __name__ == "__main__":

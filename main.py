aa151db84a42ce6bf5b9702125faf6badc7cc362
Alexandra Bentley <abent97@wgu.edu>
2026-02-03 18:26:51 -0800

    Run test_regression_model locally with pinned env and prod model

--- main.py
+++ main.py
@@ -127,20 +127,16 @@ def go(config: DictConfig):
 
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            """_ = mlflow.run(
-                f"{config['main']['components_repository']}/test_regression_model",
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 env_manager="conda",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
-                    "test_artifact": "test_data.csv:latest",
+                    "test_dataset": "test_data.csv:latest",
                 },
-            )"""
+            )
+
 
 
 
